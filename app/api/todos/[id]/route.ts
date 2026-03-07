1a1911352280e8983693c254a2e804f23bf3a96b
DatPHP <nguyenvandat170296@gmail.com>
2026-03-07 16:16:55 +0800

    Fix delete item gain

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -1,10 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-interface Params {
-    params: { id: string };
-  }
-
 export async function PUT(req: Request, { params }: any) {
   const body = await req.json();
 
@@ -16,21 +12,10 @@ export async function PUT(req: Request, { params }: any) {
   return NextResponse.json(todo);
 }
 
-export async function DELETE(req: Request, { params }: Params) {
-    if (!params?.id) {
-      return NextResponse.json({ error: "Missing id" }, { status: 400 });
-    }
-  
-    try {
-      await prisma.todo.delete({
-        where: { id: params.id },
-      });
-      return NextResponse.json({ success: true });
-    } catch (error: any) {
-      console.error(error);
-      return NextResponse.json(
-        { error: "Delete failed" },
-        { status: 500 }
-      );
-    }
-  }
+export async function DELETE(req: Request, { params }: any) {
+  await prisma.todo.delete({
+    where: { id: params.id },
+  });
+
+  return NextResponse.json({ success: true });
+}
