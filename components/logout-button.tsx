7d11af587fa12e605a08c729d89a8a636fd11d3e
DatPHP <nguyenvandat170296@gmail.com>
2026-03-15 10:34:13 +0800

    Adjust logout btn more

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -14,7 +14,7 @@ export default function LogoutButton({ className = "mb-4" }: { className?: strin
     <div className={`flex justify-end ${className}`}>
       <button
         onClick={handleLogout}
-        className="bg-red-500 hover:bg-red-600 text-white p-2 rounded flex items-center justify-center"
+        className="bg-cyan-500 hover:bg-cyan-600 text-white p-2 rounded flex items-center justify-center"
       >
         <img
           src="/logout-svgrepo-com.svg"
